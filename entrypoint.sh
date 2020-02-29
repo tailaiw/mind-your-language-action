582c934326bf92b451a43aa5c37f14d65833b6ad
tailaiw <29800495+tailaiw@users.noreply.github.com>
2020-02-28 20:19:33 -0600

    refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 
-echo "Hello $1"
+echo ::set-output name=who::$1
 time=$(date)
-echo ::set-output name=time::$time
+echo ::set-output name=when::$time
+echo ::set-output name=what::$2
