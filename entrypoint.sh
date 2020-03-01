6104e06cd6178af8247cd940b8e37a7b1e7d5e3e
tailaiw <29800495+tailaiw@users.noreply.github.com>
2020-02-29 22:01:07 -0600

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,11 @@ comment=$(cat $GITHUB_EVENT_PATH | jq -r .comment.body)
 profanity=$(python3 /check.py $comment)
 
 if [ "$profanity" = "1" ]; then
+    echo "$author said $comment"
     echo "$author said something bad"
     reply=$(python3 /react.py $author)
     echo $reply
 else
+    echo "$author said $comment"
     echo "$author said something good"
 fi
