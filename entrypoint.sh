105c7716b6643fa7c9026a04551b8cca467b450e
tailaiw <29800495+tailaiw@users.noreply.github.com>
2020-02-29 20:56:40 -0600

    move actions to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh -l
 
-profanity=$(python3 /main.py $1)
-echo ::set-output name=profanity::$profanity
+author=$1
+comment=$2
+
+profanity=$(python3 /main.py $2)
+
+if [ "$profanity" = "1" ]; then
+    echo "@$author said something bad"
+else
+    echo "@$author said something good"
+fi
