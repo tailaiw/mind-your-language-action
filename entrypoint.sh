6f814d412ac593a16e71daf3cadb01bd81e0e5f2
tailaiw <29800495+tailaiw@users.noreply.github.com>
2020-02-29 22:08:50 -0600

    reply

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,19 @@
 
 author=$(cat $GITHUB_EVENT_PATH | jq -r .comment.user.login)
 comment=$(cat $GITHUB_EVENT_PATH | jq -r .comment.body)
+issue_comments_url=$(cat $GITHUB_EVENT_PATH | jq -r .issue.comments_url)
 
 profanity=$(python3 /check.py "$comment")
 
 if [ "$profanity" = "1" ]; then
-    echo "$author said $comment"
     echo "$author said something bad"
     reply=$(python3 /react.py $author)
-    echo $reply
+    curl --include --verbose --fail \
+    -H "Accept: application/json" \
+    -H "Content-Type:application/json" \
+    -H "Authorization: token ${GITHUB_TOKEN}" \
+    --request POST --data "{body: "$reply"}" \
+    $issue_comments_url
 else
-    echo "$author said $comment"
     echo "$author said something good"
 fi
